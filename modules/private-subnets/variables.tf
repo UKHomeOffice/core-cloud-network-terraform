61c3b957dd108e83e8a8a845eac06e2ec1126521
Vijay Jadhav <mail2vjadhav@gmail.com>
2025-08-29 13:04:14 +0100

    CCL-2633: Need 3 natg - one in each zone (#2)
    * CCL-2633: Need 3 natg - one in each zone
    
    Creating Private Natg and Route Tables
    
    * Only set cluster tags if cluster is specified.
    
    ---------
    
    Co-authored-by: Samiwel Thomas <samiwel.thomas@digital.homeoffice.gov.uk>

--- modules/private-subnets/variables.tf
+++ modules/private-subnets/variables.tf
@@ -8,3 +8,18 @@ variable "tags" {
   type        = map(string)
   description = "Tags to apply to AWS resources"
 }
+
+variable "eks_cluster1_name" {
+  description = "EKS Cluster 1 Name"
+  type        = string
+}
+
+variable "eks_cluster2_name" {
+  description = "EKS Cluster 2 Name"
+  type        = string
+}
+
+variable "tgw_id" {
+  description = "TGW Id"
+  type        = string
+}
