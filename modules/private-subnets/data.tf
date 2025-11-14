f9dd686aaf01a70d8c41637594bad6890c9a0949
Ed Bent <109971677+edbentho@users.noreply.github.com>
2025-11-14 08:34:52 +0000

    revert changes and update data (#4)

--- modules/private-subnets/data.tf
+++ modules/private-subnets/data.tf
@@ -1,4 +1,3 @@
-
 # Fetch VPC ID based on its Name tag
 data "aws_vpcs" "filtered_vpcs" {
   filter {
@@ -11,18 +10,16 @@ data "aws_vpc" "selected" {
   id = data.aws_vpcs.filtered_vpcs.ids[0]
 }
 
-
-
 data "aws_availability_zones" "available" {}
 
 data "aws_region" "current" {}
 
 data "aws_vpc_endpoint" "s3" {
   vpc_id       = data.aws_vpc.selected.id
-  service_name = "com.amazonaws.${data.aws_region.current.name}.s3"
+  service_name = "com.amazonaws.${data.aws_region.current.region}.s3"
 }
 
 data "aws_vpc_endpoint" "dynamodb" {
   vpc_id       = data.aws_vpc.selected.id
-  service_name = "com.amazonaws.${data.aws_region.current.name}.dynamodb"
+  service_name = "com.amazonaws.${data.aws_region.current.region}.dynamodb"
 }
